04cc931e40946e01d6fdfbc630d306af098e604f
Sabari <mailme@isabarirajan.com>
2023-06-08 04:16:55 +0530

    Added DrawerNav and Restaurant MapScreen

---
+++ src/screens/RestaurantMapScreen.js
@@ -0,0 +1,11 @@
+import {React} from "react";
+import { Text, View } from "react-native";
+
+
+export default function RestaurantmapScreen(){
+    return (
+        <View>
+            <Text>RestaurantMapScreen</Text>
+        </View>
+    )
+}
