aa341f7def55461fdb57cbcbb7b9478b461c7df2
Sabari <mailme@isabarirajan.com>
2023-06-08 16:39:02 +0530

    Just Note

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    // [
+    //   require.resolve("react-native-reanimated/plugin")
+    // ],
     [
       require.resolve('babel-plugin-module-resolver'),
       {
