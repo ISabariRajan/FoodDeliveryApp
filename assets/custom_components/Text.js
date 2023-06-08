ac53beff93f510f27720684d67621cc66f4e2508
Sabari <mailme@isabarirajan.com>
2023-06-09 01:03:29 +0530

    Update new Text CustomComponent

--- assets/custom_components/Text.js
+++ assets/custom_components/Text.js
@@ -23,3 +23,51 @@ export const HeaderText = ({
         </View>
     )
 }
+
+export const CustomText = ({children, style, textType, ...rest}) => {
+    switch (textType){
+        case "bold":
+            style = {
+                fontWeight: "bold",
+                ...style
+            }
+            break;
+        case "inverse":
+            style = {
+                color: COLORS.inverse,
+                ...style
+            }
+            break;
+        case "bold-inverse":
+            style = {
+                color: COLORS.inverse,
+                fontWeight: "bold",
+                ...style
+            }
+            break;
+    }
+    console.log(style)
+    return (
+        <Text style={style} {...rest} >
+            {children}
+        </Text>
+    )
+}
+
+export const BoldText = ({children, style, ...rest}) => {
+    return (
+        <CustomText children={children} style={style} textType={"bold"} {...rest}/>
+    )
+}
+
+export const InverseText = ({children, style, ...rest}) => {
+    return (
+        <CustomText children={children} style={style} textType={"inverse"} {...rest}/>
+    )
+}
+
+export const BoldInverseText = ({children, style, ...rest}) => {
+    return (
+        <CustomText children={children} style={style} textType={"bold-inverse"} {...rest}/>
+    )
+}
